cbc9bee2c9d5e8e1d90b0e08cb42dfed3beca0aa
nickoval <mykovalmail@gmail.com>
2022-10-26 21:28:36 +0300

    Update 02-timer.js
    Работает только в глобальных переменных

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -15,95 +15,14 @@ const TIME_STEP = 1000;
 
 // Только глобальные переменные и функции
 // ------------------------------------------------------------------------------------------
-// let deltaTime = 0;
-// let timerID = null;
-
-// const options = {
-//   enableTime: true,
-//   time_24hr: true,
-//   defaultDate: new Date(),
-//   minuteIncrement: 1,
-
-//   onClose(selectedDates) {
-//     if (selectedDates[0] <= options.defaultDate) {
-//       Notiflix.Notify.failure(
-//         'You are not Marty McFlay! Please choose a date in the future'
-//       );
-//       return;
-//     }
-
-//     deltaTime = selectedDates[0] - options.defaultDate;
-//     onTimerStop();
-//     onDateSet();
-//     refs.btnStart.classList.add('active');
-//   },
-// };
-
-// flatpickr(refs.input, options);
-
-// function onDateSet() {
-//   refs.btnStart.addEventListener('click', onTimerWork);
-// }
-
-// function onTimerWork() {
-//   // console.log('Start!!!');
-//   onShowTime(0);
-
-//   timerID = setInterval(() => {
-//     if (deltaTime < TIME_STEP) {
-//       return;
-//     }
-//     refs.btnStart.classList.remove('active');
-//     refs.btnStart.removeEventListener('click', onTimerWork);
-//     deltaTime -= TIME_STEP;
-//     onShowTime(deltaTime);
-//   }, TIME_STEP);
-// }
-
-// function onTimerStop() {
-//   clearInterval(timerID);
-// }
-
-// function onShowTime(time) {
-//   const { days, hours, minutes, seconds } = convertMs(time);
-//   refs.days.textContent = addLeadingZero(days);
-//   refs.hours.textContent = addLeadingZero(hours);
-//   refs.minutes.textContent = addLeadingZero(minutes);
-//   refs.seconds.textContent = addLeadingZero(seconds);
-// }
-
-// function addLeadingZero(value) {
-//   return String(value).padStart(2, '0');
-// }
-
-// function convertMs(ms) {
-//   // Number of milliseconds per unit of time
-//   const second = 1000;
-//   const minute = second * 60;
-//   const hour = minute * 60;
-//   const day = hour * 24;
-//   // Remaining days
-//   const days = Math.floor(ms / day);
-//   // Remaining hours
-//   const hours = Math.floor((ms % day) / hour);
-//   // Remaining minutes
-//   const minutes = Math.floor(((ms % day) % hour) / minute);
-//   // Remaining seconds
-//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-//   return { days, hours, minutes, seconds };
-// }
-// ===========================================================================
-
-// С использованием Объекта timer
-// ---------------------------------------------------------------------------------------
+let deltaTime = 0;
+let timerID = null;
 
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-  // deltaTime: 0,
 
   onClose(selectedDates) {
     if (selectedDates[0] <= options.defaultDate) {
@@ -113,50 +32,37 @@ const options = {
       return;
     }
 
-    // deltaTime = selectedDates[0] - options.defaultDate;
-    // console.log('onClose ~ options.defaultDate', options.defaultDate);
-    // console.log('onClose ~ selectedDates[0]', selectedDates[0]);
-    timer.onTimerStop();
-    timer.onDateSet(selectedDates[0], options.defaultDate);
+    deltaTime = selectedDates[0] - options.defaultDate;
+    onTimerStop();
+    onDateSet();
     refs.btnStart.classList.add('active');
   },
 };
 
 flatpickr(refs.input, options);
 
-const timer = {
-  timerID: null,
-  deltaTime: 0,
+function onDateSet() {
+  refs.btnStart.addEventListener('click', onTimerWork);
+}
 
-  onDateSet(dateStart, dateStop) {
-    this.deltaTime = dateStart - dateStop;
-    // console.log('onDateSet ~ dateStart', dateStart);
-    // console.log('onDateSet ~ dateStop', dateStop);
-    refs.btnStart.addEventListener('click', timer.onTimerWork.bind(this));
-  },
+function onTimerWork() {
+  // console.log('Start!!!');
+  onShowTime(0);
 
-  onTimerWork() {
-    // console.log('Start!!!');
-    onShowTime(0);
-    // console.log('this.deltaTime :', this.deltaTime);
+  timerID = setInterval(() => {
+    if (deltaTime < TIME_STEP) {
+      return;
+    }
+    refs.btnStart.classList.remove('active');
+    refs.btnStart.removeEventListener('click', onTimerWork);
+    deltaTime -= TIME_STEP;
+    onShowTime(deltaTime);
+  }, TIME_STEP);
+}
 
-    this.timerID = setInterval(() => {
-      if (this.deltaTime < TIME_STEP) {
-        return;
-      }
-      // console.log('this.timerID: ', this.timerID);
-      refs.btnStart.classList.remove('active');
-      refs.btnStart.removeEventListener('click', timer.onTimerWork.bind(this));
-      this.deltaTime -= TIME_STEP;
-      // console.log('this.timerID=setInterval ~ this.deltaTime', this.deltaTime);
-      onShowTime(this.deltaTime);
-    }, TIME_STEP);
-  },
-
-  onTimerStop() {
-    clearInterval(this.timerID);
-  },
-};
+function onTimerStop() {
+  clearInterval(timerID);
+}
 
 function onShowTime(time) {
   const { days, hours, minutes, seconds } = convertMs(time);
@@ -187,3 +93,97 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
+// ===========================================================================
+
+// С использованием Объекта timer (не снимается слушатель с кнопки)
+// ---------------------------------------------------------------------------------------
+
+// const options = {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   // deltaTime: 0,
+
+//   onClose(selectedDates) {
+//     if (selectedDates[0] <= options.defaultDate) {
+//       Notiflix.Notify.failure(
+//         'You are not Marty McFlay! Please choose a date in the future'
+//       );
+//       return;
+//     }
+
+//     // deltaTime = selectedDates[0] - options.defaultDate;
+//     // console.log('onClose ~ options.defaultDate', options.defaultDate);
+//     // console.log('onClose ~ selectedDates[0]', selectedDates[0]);
+//     timer.onTimerStop();
+//     timer.onDateSet(selectedDates[0], options.defaultDate);
+//     refs.btnStart.classList.add('active');
+//   },
+// };
+
+// flatpickr(refs.input, options);
+
+// const timer = {
+//   timerID: null,
+//   deltaTime: 0,
+
+//   onDateSet(dateStart, dateStop) {
+//     this.deltaTime = dateStart - dateStop;
+//     // console.log('onDateSet ~ dateStart', dateStart);
+//     // console.log('onDateSet ~ dateStop', dateStop);
+//     refs.btnStart.addEventListener('click', timer.onTimerWork.bind(this));
+//   },
+
+//   onTimerWork() {
+//     // console.log('Start!!!');
+//     onShowTime(0);
+//     // console.log('this.deltaTime :', this.deltaTime);
+
+//     this.timerID = setInterval(() => {
+//       if (this.deltaTime < TIME_STEP) {
+//         return;
+//       }
+//       console.log('this.timerID: ', this.timerID);
+//       refs.btnStart.classList.remove('active');
+//       refs.btnStart.removeEventListener('click', timer.onTimerWork.bind(this));
+//       this.deltaTime -= TIME_STEP;
+//       // console.log('this.timerID=setInterval ~ this.deltaTime', this.deltaTime);
+//       onShowTime(this.deltaTime);
+//     }, TIME_STEP);
+//   },
+
+//   onTimerStop() {
+//     clearInterval(this.timerID);
+//   },
+// };
+
+// function onShowTime(time) {
+//   const { days, hours, minutes, seconds } = convertMs(time);
+//   refs.days.textContent = addLeadingZero(days);
+//   refs.hours.textContent = addLeadingZero(hours);
+//   refs.minutes.textContent = addLeadingZero(minutes);
+//   refs.seconds.textContent = addLeadingZero(seconds);
+// }
+
+// function addLeadingZero(value) {
+//   return String(value).padStart(2, '0');
+// }
+
+// function convertMs(ms) {
+//   // Number of milliseconds per unit of time
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
+//   // Remaining days
+//   const days = Math.floor(ms / day);
+//   // Remaining hours
+//   const hours = Math.floor((ms % day) / hour);
+//   // Remaining minutes
+//   const minutes = Math.floor(((ms % day) % hour) / minute);
+//   // Remaining seconds
+//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+//   return { days, hours, minutes, seconds };
+// }
