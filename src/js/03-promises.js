4b3a3dd42b2354a8db37e2e55d50c99d65a387d0
nickoval <mykovalmail@gmail.com>
2022-10-26 19:51:01 +0300

    Задача 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -4,12 +4,8 @@ const refs = {
   // delay : document.querySelector('[name="delay"]'),
   // step : document.querySelector('[name="step"]'),
   // amount : document.querySelector('[name="amount"]'),
-  form : document.querySelector('.form')
-}
-
-// refs.delay.addEventListener('', ()=>{} );
-// refs.step.addEventListener('', ()=>{} );
-// refs.amount.addEventListener('', ()=>{} );
+  form: document.querySelector('.form'),
+};
 
 // refs.form.addEventListener('submit', (evt)=>{
 //   evt.preventDefault();
@@ -21,131 +17,112 @@ const refs = {
 //   console.log('amount: ',amount);
 // } );
 
-refs.form.addEventListener('submit', onSubmit );
+refs.form.addEventListener('submit', onSubmit);
 
 function onSubmit(evt) {
   evt.preventDefault();
+
   const startDelay = Number(evt.currentTarget.elements.delay.value);
-  console.log('delay: ',startDelay);
   const step = Number(evt.currentTarget.elements.step.value);
-  console.log('step: ',step);
   const amount = Number(evt.currentTarget.elements.amount.value);
-  console.log('amount: ',amount);
 
-  for (let i = 0; i < amount; i++) {
-    const delay = startDelay + i*step
+  // Для варианта 1
+  // for (let i = 0; i < amount; i++) {
+  //   const delay = startDelay + i * step;
+  //   runPromise(i, delay);
+  // }
 
-    // runPromise(i, delay);
-    // initPromise(i, delay);
+  // Для варианта 2
+  // initPromise(startDelay, step, amount);
 
-
-    const promise = new Promise((resolve, reject) => {
-      setTimeout(() => {
-        const shouldResolve = Math.random() > 0.3;
-        if (shouldResolve) {
-          onFulfill();
-  
-        } else {
-          onReject();
-        }
-      }, delay);
-    });
-
-    promise.then(
-      // onResolve will run third or not at all
-      value => {
-        onFulfill(position, delay)
-      },
-      // onReject will run third or not at all
-      error => {
-        onReject(position, delay)
-      }
-    );
-    
-  }
-}
-console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
-
-// function runPromise(position, delay) {
-//   const promise = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       const shouldResolve = Math.random() > 0.3;
-//       if (shouldResolve) {
-//         onFulfill();
-
-//       } else {
-//         onReject();
-//       }
-//     }, delay);
-//   });
-//   return promise;
-// }
-
-
-  // promise.then(
-  //   // onResolve will run third or not at all
-  //   value => {
-  //     onFulfill(position, delay)
-  //   },
-  //   // onReject will run third or not at all
-  //   error => {
-  //     onReject(position, delay)
-  //   }
-  // );
-
-// initPromise(onFulfill, onReject);
-
-function onFulfill(position, delay) {        Notiflix.Notify.success(
-  `✅ Fulfilled promise ${position} in ${delay}ms`
-);
-console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  
+  // Для варианта 3
+  promiseCounter(startDelay, step, amount);
 }
 
-function onReject(position, delay) {
-  Notiflix.Notify.failure(
-    `❌ Rejected promise ${position} in ${delay}ms`
-  );
-  console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-}
-
+// Работает без всяких промисов (вариант 1)
+// --------------------------------------------------------------
 // function runPromise(position, delay) {
 //   console.log('position: ', position);
-//   console.log('delay: ',delay);
-//   // return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       const shouldResolve = Math.random() > 0.3;
-//       if (shouldResolve) {
-//         Notiflix.Notify.success(
-//           `✅ Fulfilled promise ${position} in ${delay}ms`
-//         );
-//         console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//       } else {
-//         Notiflix.Notify.failure(
-//           `❌ Rejected promise ${position} in ${delay}ms`
-//         );
-//         console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//       }
-//     }, delay);
-//   // });
+//   console.log('delay: ', delay);
+//   setTimeout(() => {
+//     const shouldResolve = Math.random() > 0.3;
+//     if (shouldResolve) {
+//       Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//       console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//     } else {
+//       Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+//       console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//     }
+//   }, delay);
 // }
+// ==================================================================================
 
-// function createPromise(position, delay) {
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     // Fulfill
-//   } else {
-//     // Reject
+// Одна большая функция со сетчиком и промисом (вариант 2) (!!!Не передаются данные под вывод!!!)
+// -----------------------------------------------------------------------------
+// function initPromise(startDelay, step, amount) {
+//   for (let i = 0; i < amount; i++) {
+//     const delay = startDelay + i * step;
+
+//     const promise = new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         const shouldResolve = Math.random() > 0.3;
+//         if (shouldResolve) {
+//           onFulfill();
+//         } else {
+//           onReject();
+//         }
+//       }, delay);
+//     });
+
+//     promise.then(
+//       value => {
+//         onFulfill(position, delay);
+//       },
+//       error => {
+//         onReject(position, delay);
+//       }
+//     );
 //   }
 // }
 
+// function onFulfill(position, delay) {
+//   Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+// }
 
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
+// function onReject(position, delay) {
+//   Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+// }
+// ====================================================================================
 
+// Вариант 3 Немного похож на Репетовский (функции разделены, аргументы передаются из функции в функцию)
+// -------------------------------------------------------------------------------------
+function promiseCounter(startDelay, step, amount) {
+  for (let i = 0; i < amount; i++) {
+    let delay = startDelay + i * step;
+    createPromise(i, delay).then(onFulfill).catch(onReject);
+  }
+}
 
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onFulfill({ position, delay }) {
+  Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+}
+
+function onReject({ position, delay }) {
+  Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+}
+// ====================================================================================
